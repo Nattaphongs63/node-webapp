89340b1d64a4e2692e5a9af3508cc5faf18bde4e
nattaphongs63 <nattaphongs63@nu.ac.th>
2022-07-31 20:15:04 +0700

    codelab5

--- app.js
+++ app.js
@@ -9,14 +9,20 @@ var util = require('./utility');
 var app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+/** specify the directory from where to serve static assets such as JavaScript, CSS, images **/
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+app.use('/jquery-ui', express.static(__dirname + '/node_modules/jquery-ui/dist/'));
+
+/** remove fix route and use path solution **/
 /**
  Create my-route
 **/
 
-app.get('/', function(req, res) {
+/**app.get('/', function(req, res) {
     res.sendFile('public/index.html', { root: __dirname });
-});
+});**/
 
 app.get('/welcome', (req, res) =>
   res.send({
@@ -25,7 +31,7 @@ app.get('/welcome', (req, res) =>
   }),
 );
 
-app.post('/', function(req, res) {
+app.post('/data', function(req, res) {
    
    var result ={ student_id: req.body.student_id,
      firstname: req.body.firstname,
@@ -47,8 +53,9 @@ app.get('/ok', (req, res) =>{
 
 
 app.get('/student/:student_id', function(req, res) {
-    util.fakeStudentbyInfo(req.params.student_id, function (result) {
-        res.json(result);
+    util.findStudentbyId(req.params.student_id, function (result) {
+        //res.json(result);
+        res.send(result);
     });
 });
 
@@ -57,4 +64,4 @@ var port = process.env.PORT || 3000;
 
 app.listen(port, function () {
     console.log('Starting node.js on port ' + port);
-});
+}); 
--- utility.js
+++ utility.js
@@ -1,8 +1,8 @@
 //utility.js
 
 const request = require("request-promise");
-const EXTERNAL_API=""; // put url
-const accessToken=""; // put access token
+const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjU5MjcyNDA5LCJleHAiOjE2NTkyNzYwMDksIm5iZiI6MTY1OTI3MjQwOSwianRpIjoiNkMxb0NtbXJzc0ZtQ3g5ViIsInN1YiI6MTI1LCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.o3J9vZu7-jZg4bx1bwXHSWW2_BVX0NOZjl_3yYmBBn0"; // put access token
 const student = {
   name: '', // replace with your full name.
   age: 19, // put your age.
@@ -12,33 +12,29 @@ const student = {
 
 
 exports.findStudentbyId = function (student_id, cb) {
-  //-- call external api 
-          request({
-            method: "POST",
-            uri: EXTERNAL_API,
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            },
-            formData: {
-                message: `HTTP Request :${data.student_id} `,
-                student_id: student_id
-            }
-        }).then((response) => {
-            console.log('Sent');
-            console.log({
-                "request": student_id,
-                "response": response
-            });
-
-           cb(response);
-
-        }).catch((err) => {
-            console.log('Error:', err.message);
-            console.log({
-                "Error": err.message
-            });
-        });
-}
+    //-- call external api 
+            request({
+              method: "GET",
+              uri: EXTERNAL_API+student_id,
+              headers: {
+                  Authorization: `Bearer ${accessToken}`
+              }
+          }).then((response) => {
+              console.log('Sent');
+              console.log({
+                  "request": student_id,
+                  "response": response
+              });
+  
+             cb(response);
+  
+          }).catch((err) => {
+              console.log('Error:', err.message);
+              console.log({
+                  "Error": err.message
+              });
+          });
+  }
 
 exports.fakeStudentbyInfo = function (student_id, cb) {
 
